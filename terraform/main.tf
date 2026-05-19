5ca32a16bee598636c408371316fcb0a6a2b9c91
jamesadewara <jamesadewara1@gmail.com>
2026-05-19 10:25:19 +0000

    formated

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "s3" {
-    key    = "infrastructure/terraform.tfstate"
-    region = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    key          = "infrastructure/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
